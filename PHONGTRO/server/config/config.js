c49119013bacbabcbb0eb86c3854b90fa5e00ad2
Võ Thị Kiều Oanh <21dh111358@st.huflit.edu.vn>
2023-10-19 22:46:20 +0700

    first commit

---
+++ PHONGTRO/server/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const password = process.env.DB_PASSWORD || null
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: password,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: 3306,
+        dialect: process.env.DB_DIALECT,
+        logging: false
+        // dialectOptions: {
+        //   bigNumberStrings: true
+        // }
+    },
+};
